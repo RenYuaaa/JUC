d45e780ff28fddd211fd3473cf79ffc3d8868e20
zhangxiaotong <zhangxiaotong@zhangxiaotongdeMacBook-Pro.local>
2021-09-08 17:47:33 +0800

    add notice

--- src/main/java/com/study/ren/juc/c_015/T.java
+++ src/main/java/com/study/ren/juc/c_015/T.java
@@ -1,16 +1,12 @@
-package com.study.ren.juc.c_014;
+package com.study.ren.juc.c_015;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * volatile并不能保证多个线程共同修改running变量时所带来的不一致问题
- * 也就是说volatile不能替代synchronized
- * volatile只能保证可见性，不能保证原子性
- */
 public class T {
+
     volatile int count = 0;
-    void m() {
+    synchronized void m() {
         for (int i = 0; i < 10000; i++) {
             count++;
         }
